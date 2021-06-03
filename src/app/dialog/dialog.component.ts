f865784b9dc37e5d1a755cddaee2bf6e6164cc83
Alisson Henrique <alisson@pdsolucoes.com.br>
2021-06-03 11:39:59 -0300

    feat: add dialog component

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogConfig } from '../shared/interfaces/dialog-config.interface';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css'],
+})
+export class DialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogConfig
+  ) { }
+
+  onOkClick(): void {
+    this.dialogRef.close();
+  }
+}

45bafaf4b4af31921fe57d2b69559799bf99b767
Alisson Henrique <alisson@pdsolucoes.com.br>
2021-06-02 22:22:44 -0300

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: LoginComponent, pathMatch: 'full', canActivate: [ActiveWhenNotLogged] },
+  //{ path: 'login', component: LoginComponent, canActivate: [ActiveWhenNotLogged] },
+  { path: '', component: LoginComponent, pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    //canActivate: [ActiveWhenLogged],
+    children: [
+      // {
+      //   path: 'users',
+      //   component: UserComponent
+      // },
+      // {
+      //   path: 'users/new',
+      //   component: UserCreateComponent
+      // },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

734b7ec934dccfa4099805954789b5d3f9364ce0
Alisson Henrique <alisson@pdsolucoes.com.br>
2021-06-15 22:25:58 -0300

    refactor: correction app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,57 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { NavComponent } from './views/nav/nav.component';
 import { ItemMenuComponent } from './views/item-menu/item-menu.component';
 import { ItemMenulChildDirective } from './views/item-menu/item-menu-child.directive';
+import { CategoryComponent } from './category/category.component';
+import { CategoryCreateComponent } from './category/category-create/category-create.component';
+import { DialogComponent } from './views/dialog/dialog.component';
+import { FieldsComponent } from './views/fields/fields.component';
+import { NotifierComponent } from './snack-bar/notifier/notifier.component';
+import { TableComponent } from './views/table/table.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CategoryService } from './shared/services/category.service';
+import { AuthInterceptor, AuthService } from './shared/services/auth.service';
+import { MovementComponent } from './movement/movement.component';
+import { MovementCreateComponent } from './movement/movement-create/movement-create.component';
+import { MovementService } from './shared/services/movement.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
+    TableComponent,
+    DialogComponent,
+    FieldsComponent,
+    CategoryComponent,
+    CategoryCreateComponent,
+    NotifierComponent,
     NavComponent,
     ItemMenuComponent,
-    ItemMenulChildDirective
+    ItemMenulChildDirective,
+    DashboardComponent,
+    MovementComponent,
+    MovementCreateComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule,
+    //FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    CategoryService,
+    MovementService,
+    AuthService,
+   // ActiveWhenLogged,
+    //ActiveWhenNotLogged,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

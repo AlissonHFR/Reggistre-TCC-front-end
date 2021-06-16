41d2a93dd3b76bd552510ac803f84c62bf8bc61e
Alisson Henrique <alisson@pdsolucoes.com.br>
2021-06-15 22:14:06 -0300

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryCreateComponent } from './category/category-create/category-create.component';
+import { CategoryComponent } from './category/category.component';
+import { MovementCreateComponent } from './movement/movement-create/movement-create.component';
+import { MovementComponent } from './movement/movement.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { LoginComponent } from './views/login/login.component';
 import { NavComponent } from './views/nav/nav.component';
@@ -26,6 +30,22 @@ const routes: Routes = [
       //   path: 'users/new',
       //   component: UserCreateComponent
       // },
+      {
+        path: 'category',
+        component: CategoryComponent
+      },
+      {
+        path: 'category/new',
+        component: CategoryCreateComponent
+      },
+      {
+        path: 'movement',
+        component: MovementComponent
+      },
+      {
+        path: 'movement/new',
+        component: MovementCreateComponent
+      },
     ]
   }
 ];
